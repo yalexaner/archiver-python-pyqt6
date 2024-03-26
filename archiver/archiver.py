d06ef26b4cd3d0056e9f043888c5de216a717ae1
yalexaner <mail@yalexaner.ru>
2024-03-26 19:04:49 +0500

    use default compression algorithm

--- archiver/archiver.py
+++ archiver/archiver.py
@@ -6,7 +6,7 @@ def archive_file(zip_file_path, file_path):
     if not file_path:
         return
 
-    with zipfile.ZipFile(zip_file_path, "a") as zip_file:
+    with zipfile.ZipFile(zip_file_path, "a", compression=zipfile.ZIP_DEFLATED, compresslevel=6) as zip_file:
         filename = os.path.basename(file_path)
         zip_file.write(file_path, arcname=filename)
 
@@ -16,7 +16,7 @@ def archive_directory(zip_file_path, dir_path):
         return
 
     dir_name = os.path.basename(dir_path)
-    with zipfile.ZipFile(zip_file_path, "a") as zip_file:
+    with zipfile.ZipFile(zip_file_path, "a", compression=zipfile.ZIP_DEFLATED, compresslevel=6) as zip_file:
         for root, dirs, files in os.walk(dir_path):
             for file in files:
                 file_path = os.path.join(root, file)
